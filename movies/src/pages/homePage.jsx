85a2e9a7d10b0494c04e4714b4cdd958b659783b
tomas <tomasmcgrath21@gmail.com>
2025-11-02 20:09:39 +0000

    Added pagination to home page by slicing movies

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import Pagination from '@mui/material/Pagination'; // MUI pagination
 
 
 const HomePage = (props) => {
@@ -12,6 +13,11 @@ const HomePage = (props) => {
     queryKey: ['discover'],
     queryFn: getMovies,
   })
+
+  const [page, setPage] = useState(1);
+   const moviesPerPage = 10;
+   
+
   
   if (isPending) {
     return <Spinner />
@@ -23,19 +29,33 @@ const HomePage = (props) => {
   
   const movies = data.results;
 
+
+  const startIndex = (page - 1) * moviesPerPage;
+  const paginatedMovies = movies.slice(startIndex, startIndex + moviesPerPage);
+
+    const handlePageChange = (event, value) => {
+    setPage(value);
+  }
+
+
+
   // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
   const addToFavorites = (movieId) => true 
 
     return (
-      <PageTemplate
+ 
+      <><PageTemplate
         title="Discover Movies"
-        movies={movies}
-        action={(movie) => {
-          return <AddToFavoritesIcon movie={movie} />
-        }}
-      />
+        movies={paginatedMovies}
+        action={(movie) => <AddToFavoritesIcon movie={movie} />} /><Pagination
+          count={Math.ceil(movies.length / moviesPerPage)}
+          page={page}
+          onChange={handlePageChange}
+          color="primary"
+          sx={{ display: 'flex', justifyContent: 'center' }} /></>
+    
   );
 
 };
